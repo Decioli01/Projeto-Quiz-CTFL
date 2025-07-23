fa4dfd38fc9e21a0a0d67c45493139682234bafd
Eduardo Decioli <eduardo.decioli01@gmail.com>
2025-07-23 15:37:38 -0300

    Criado botão para mostrar questões erradas
    Criado botão 'Ver Respostas Erradas' para conferir as respostas selecionadas erradas e também implementado para selecionar as questões randomicamente.

--- CTFL/script.js
+++ CTFL/script.js
@@ -3,12 +3,12 @@
 let currentQuestion = 0;
 let points = 0;
 var timer;
+const respostasErradas = [];
 
 let scoreArea = document.querySelector('.scoreArea button')
-if(scoreArea){
+if (scoreArea) {
     scoreArea.addEventListener('click', resetEvent)
 }
-
 
 //função do botão iniciar
 function comecar() {
@@ -18,15 +18,16 @@ function comecar() {
     startTimer();
     showQuestion();
 }
+
 //função para começar o timer
-function startTimer(){
+function startTimer() {
     var presentTime = document.getElementById("timer").innerHTML;
     var timeArray = presentTime.split(":");
     var minutes = timeArray[0];
     var seconds = timeArray[1];
     var totalSeconds = minutes * 60 + parseInt(seconds);
-  
-    timer = setInterval(function() {
+
+    timer = setInterval(function () {
         if (totalSeconds <= 0) {
             clearInterval(timer);
             alert("Timer finalizado!");
@@ -39,58 +40,73 @@ function startTimer(){
         }
         document.getElementById("timer").innerHTML = minutes + ":" + seconds;
         totalSeconds--;
-  }, 1000);
+    }, 1000);
 }
-function stopTimer(){
+function stopTimer() {
     clearInterval(timer);
 }
+
 //função para mostrar as questões;
-function showQuestion(){
-    if (questions[currentQuestion]){
-        let q = questions[currentQuestion];
-        
-        //calcula porcentagem para fazer a animação na barra de progresso avançar conforme avança as questões
-        let pct = Math.floor((currentQuestion / questions.length) * 100);
+let questionsShuffled = [];
+
+function shuffleQuestions() {
+    questionsShuffled = [...questions]; // copia o array original
+    for (let i = questionsShuffled.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [questionsShuffled[i], questionsShuffled[j]] = [questionsShuffled[j], questionsShuffled[i]];
+    }
+}
+
+function showQuestion() {
+    if (currentQuestion === 0) shuffleQuestions(); // embaralha no início
+
+    if (questionsShuffled[currentQuestion]) {
+        let q = questionsShuffled[currentQuestion];
+
+        let pct = Math.floor((currentQuestion / questionsShuffled.length) * 100);
         document.querySelector('.progress--bar').style.width = `${pct}%`;
 
-        //esconde a area do resultado e mostra a area das questões
         document.querySelector('.scoreArea').style.display = 'none';
         document.querySelector('.questionArea').style.display = 'block';
 
-        //monta a questão
-        document.querySelector('.question').innerHTML = parseInt(currentQuestion + 1) + ' - ' + q.question;
+        document.querySelector('.question').innerHTML = `${currentQuestion + 1} - ${q.question}`;
 
-        //colocar as alternativas
-        let optionsHTML = ''; 
+        let optionsHTML = '';
         for (let i in q.options) {
-            optionsHTML += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span> ${q.options[i]}</div>`;
+            optionsHTML += `<div data-op="${i}" class="option"><span>${parseInt(i) + 1}</span> ${q.options[i]}</div>`;
         }
         document.querySelector('.options').innerHTML = optionsHTML;
 
-        //evento de clique na opção
-        document.querySelectorAll('.options .option').forEach(item =>{
+        document.querySelectorAll('.options .option').forEach(item => {
             item.addEventListener('click', optionClickEvent);
-        })
-    }
-    else{
+        });
+
+    } else {
         finishQuiz();
     }
-}    
+}
 
 //função para avançar as questões
 function optionClickEvent(e) {
     let clickedOption = parseInt(e.target.getAttribute('data-op'));
-    console.log(clickedOption)
-    
-    if(questions[currentQuestion].answer === clickedOption) {
+    let questaoAtual = questions[currentQuestion];
+
+    if (questaoAtual.answer === clickedOption) {
         points++;
+    } else {
+        respostasErradas.push({
+            numeroQuestao: currentQuestion + 1,
+            enunciado: questaoAtual.question,
+            alternativaSelecionada: questaoAtual.options[clickedOption],
+            respostaCorreta: questaoAtual.options[questaoAtual.answer]
+        });
     }
     currentQuestion++;
     showQuestion();
 }
 
 //função para quando acabar as questões
-function finishQuiz(){
+function finishQuiz() {
     let acertos = Math.floor((points / questions.length) * 100);
 
     document.querySelector('.scorePct').innerHTML = `Acertou ${acertos}%`
@@ -100,7 +116,7 @@ function finishQuiz(){
     document.querySelector('.questionArea').style.display = 'none';
     document.querySelector('.progress--bar').style.width = '100%';
 
-    if (acertos < 65){
+    if (acertos < 65) {
         let parabens = document.querySelector('.scoreText1');
         parabens.innerHTML = 'Que pena!'
         document.querySelector('.scorePct').style.color = '#D31145'
@@ -109,8 +125,34 @@ function finishQuiz(){
     stopTimer();
 }
 //função do botão "Fazer Novamente"
-function resetEvent(){
+function resetEvent() {
     window.location.reload();
     points = 0;
     currentQuestion = 0;
+    respostasErradas.length = 0;
 }
+
+//funcao do modal de respostas erradas
+function abrirModal() {
+    const conteudo = document.getElementById("conteudoErros");
+    conteudo.innerHTML = "";
+
+    if (respostasErradas.length === 0) {
+        conteudo.innerHTML = "<p>Parabéns! Você não errou nenhuma questão.</p>";
+    } else {
+        respostasErradas.forEach((erro) => {
+            conteudo.innerHTML += `
+                <p><strong>Questão ${erro.numeroQuestao}: ${erro.enunciado}</strong></p>
+                <p>Sua resposta: <span style="color:red;">${erro.alternativaSelecionada}</span></p>
+                <p>Resposta correta: <span style="color:green;">${erro.respostaCorreta}</span></p>
+                <hr>
+            `;
+        });
+    }
+    document.getElementById("modalErros").style.display = "flex";
+}
+
+function fecharModal() {
+    document.getElementById("modalErros").style.display = "none";
+}
+
